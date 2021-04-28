04fc48c63666061e9d642d2e6a6bd3ed530f3889
garethbaars <gareth.baars@intentarchitect.com>
2021-04-28 12:25:14 +0200

    Intent.Java.Spring.Cors module

---
+++ Intent.Modules.Java.Spring.Cors/Templates/CorsConfig/CorsConfigTemplatePartial.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Intent.Engine;
+using Intent.Modules.Common.Java;
+using Intent.Modules.Common.Java.Templates;
+using Intent.Modules.Common.Templates;
+using Intent.RoslynWeaver.Attributes;
+using Intent.Templates;
+
+
+[assembly: DefaultIntentManaged(Mode.Merge)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Java.Templates.JavaFileTemplatePartial", Version = "1.0")]
+
+namespace Intent.Modules.Java.Spring.Cors.Templates.CorsConfig
+{
+    [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
+    partial class CorsConfigTemplate : JavaTemplateBase<object>
+    {
+        [IntentManaged(Mode.Fully)]
+        public const string TemplateId = "Intent.Java.Spring.Cors.CorsConfig";
+
+        [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
+        public CorsConfigTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
+        {
+        }
+
+        [IntentManaged(Mode.Merge, Body = Mode.Ignore, Signature = Mode.Fully)]
+        public override ITemplateFileConfig GetTemplateFileConfig()
+        {
+            return new JavaFileConfig(
+                className: $"CorsConfig",
+                package: $"{OutputTarget.GetPackage()}"
+            );
+        }
+
+    }
+}
