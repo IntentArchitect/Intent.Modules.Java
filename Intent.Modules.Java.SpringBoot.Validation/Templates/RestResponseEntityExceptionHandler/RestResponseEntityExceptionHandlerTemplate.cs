70a0cd2842df552884a358c5ae8c0dc49d1cf4fd
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-08-28 22:46:47 +0200

    Little snag.

--- Intent.Modules.Java.SpringBoot.Validation/Templates/RestResponseEntityExceptionHandler/RestResponseEntityExceptionHandlerTemplate.cs
+++ Intent.Modules.Java.SpringBoot.Validation/Templates/RestResponseEntityExceptionHandler/RestResponseEntityExceptionHandlerTemplate.cs
@@ -18,6 +18,7 @@ package {Package};
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
