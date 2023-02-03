b9c5f8632b01adc4b11605aa50e494eb5d710810
dandrejvv <dandrejvv@gmail.com>
2023-02-03 09:04:24 +0200

    Delete operation completed based on the idea that we return a DTO on the found entity that will be deleted. This allows us to figure out easily what the linked Entity is for the operaiton.

--- Intent.Modules.Java.Services.CRUD/Decorators/ImplementationStrategies/DeleteImplementationStrategy.cs
+++ Intent.Modules.Java.Services.CRUD/Decorators/ImplementationStrategies/DeleteImplementationStrategy.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using Intent.Engine;
 using Intent.Modelers.Domain.Api;
+using Intent.Modelers.Services.Api;
+using Intent.Modules.Common.Java.Builder;
 using Intent.Modules.Common.Templates;
+using Intent.Modules.Java.Domain.Templates.DomainModel;
 using Intent.Modules.Java.Persistence.JPA;
+using Intent.Modules.Java.Services.Templates.DataTransferModel;
 using Intent.Modules.Java.Services.Templates.ServiceImplementation;
+using Intent.Modules.Java.Spring.Data.Repositories.Templates.EntityRepository;
 using OperationModel = Intent.Modelers.Services.Api.OperationModel;
 
 namespace Intent.Modules.Java.Services.CRUD.Decorators.ImplementationStrategies
@@ -23,57 +28,74 @@ namespace Intent.Modules.Java.Services.CRUD.Decorators.ImplementationStrategies
         
         public bool IsMatch(OperationModel operationModel)
         {
-            return false;
-            // var lowerDomainName = domainModel.Name.ToLower();
-            // var lowerOperationName = operationModel.Name.ToLower();
-            // if (operationModel.Parameters.Count() != 1)
-            // {
-            //     return false;
-            // }
-            //
-            // if (!operationModel.Parameters.Any(p => string.Equals(p.Name, "id", StringComparison.InvariantCultureIgnoreCase) ||
-            //                                         string.Equals(p.Name, $"{lowerDomainName}Id", StringComparison.InvariantCultureIgnoreCase)))
-            // {
-            //     return false;
-            // }
-            //
-            // if (operationModel.TypeReference.Element != null)
-            // {
-            //     return false;
-            // }
-            //
-            // // Support for composite primary keys not implemented:
-            // if (domainModel.GetPrimaryKeys().PrimaryKeys.Count > 1)
-            // {
-            //     return false;
-            // }
-            //
-            // return new[]
-            //     {
-            //         "delete",
-            //         $"delete{lowerDomainName}"
-            //     }
-            //     .Contains(lowerOperationName);
+            if (operationModel.Parameters.Count != 0)
+            {
+                return false;
+            }
+
+            if (!operationModel.Parameters.Any(p => p.Name.Contains("id", StringComparison.OrdinalIgnoreCase)))
+            {
+                return false;
+            }
+
+            var dtoModel = operationModel.TypeReference.Element?.AsDTOModel();
+            if (dtoModel == null)
+            {
+                return false;
+            }
+
+            var domainModel = dtoModel.Mapping?.Element?.AsClassModel();
+            if (domainModel == null)
+            {
+                return false;
+            }
+            
+            // Support for composite primary keys not implemented:
+            if (domainModel.GetPrimaryKeys().PrimaryKeys.Count > 1)
+            {
+                return false;
+            }
+            
+            var lowerOperationName = operationModel.Name.ToLower();
+            return new[] { "delete", domainModel.Name }.Any(x => lowerOperationName.Contains(x));
         }
 
         public void ApplyStrategy(OperationModel operationModel)
         {
-            throw new NotImplementedException();
+            var dtoModel = operationModel.TypeReference.Element.AsDTOModel();
+            var dtoType = _template.TryGetTypeName(DataTransferModelTemplate.TemplateId, dtoModel, out var dtoName)
+                ? dtoName
+                : dtoModel.Name.ToPascalCase();
+            var domainModel = dtoModel.Mapping.Element.AsClassModel();
+            var domainType = _template.TryGetTypeName(DomainModelTemplate.TemplateId, domainModel, out var result)
+                ? result
+                : domainModel.Name;
+            var domainTypeCamelCased = domainType.ToCamelCase();
+            var domainTypePascalCased = domainType.ToPascalCase();
+            var repositoryTypeName = _template.GetTypeName(EntityRepositoryTemplate.TemplateId, domainModel);
+            var repositoryFieldName = repositoryTypeName.ToCamelCase();
+            
+            var codeLines = new JavaStatementAggregator();
+            codeLines.Add($@"var {domainTypeCamelCased} = {repositoryFieldName}.findById({operationModel.Parameters.First().Name.ToCamelCase()});");
+            codeLines.Add($@"if (!{domainTypeCamelCased}.isPresent()) {{");
+            codeLines.Add($@"   return null;");
+            codeLines.Add($@"}}");
+            codeLines.Add($@"var found{domainTypePascalCased} = {dtoType}.mapFrom{domainTypePascalCased}({domainTypeCamelCased}.get(), mapper);");
+            codeLines.Add($@"{repositoryFieldName}.delete({domainTypeCamelCased});");
+            codeLines.Add($@"return found{domainTypePascalCased};");
+            
+            var @class = _template.JavaFile.Classes.First();
+            if (@class.Fields.All(p => p.Type != repositoryTypeName))
+            {
+                @class.AddField(_template.ImportType(repositoryTypeName), repositoryFieldName);
+            }
+            if (@class.Fields.All(p => p.Type != "org.modelmapper.ModelMapper"))
+            {
+                @class.AddField(_template.ImportType("org.modelmapper.ModelMapper"), "mapper");
+            }
+            var method = @class.FindMethod(m => m.Name.Equals(operationModel.Name, StringComparison.OrdinalIgnoreCase));
+            method.Statements.Clear();
+            method.AddStatements(codeLines.ToList());
         }
-
-        // public string GetImplementation(ClassModel domainModel, OperationModel operationModel)
-        // {
-        //     var domainType = _decorator.GetDomainTypeName(domainModel);
-        //     var domainTypeCamelCased = domainType.ToCamelCase();
-        //     var repositoryFieldName = _decorator.GetRepositoryDependency(domainModel).Name;
-        //
-        //     return $@"var {domainTypeCamelCased} = {repositoryFieldName}.findById({operationModel.Parameters.Single().Name.ToCamelCase()}).get();
-        // {repositoryFieldName}.delete({domainTypeCamelCased});";
-        // }
-        //
-        // public IEnumerable<ClassDependency> GetRequiredServices(ClassModel targetEntity)
-        // {
-        //     yield return _decorator.GetRepositoryDependency(targetEntity);
-        // }
     }
 }
